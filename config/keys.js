5fda99090d369d44efc9952526ec4514790bd1bd
Kevin <danggkev@gmail.com>
2019-01-30 18:43:11 -0800

    updated commit

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-/* Makes this object available
-outside of the file */
-module.exports = {
-  mongoURI: "mongodb://kevin:a12345@ds139920.mlab.com:39920/dev-connector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
--- server.js
+++ server.js
@@ -1,36 +1,48 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const bodyParser = require("body-parser");
-const passport = require("passport");
-//DB Config
-const db = require("./config/keys").mongoURI;
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+// DB Config
+const db = require('./config/keys').mongoURI;
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose
   .connect(db)
-  //Upon successfully connecting to the DB
-  .then(() => console.log("MongoDB Connected"))
-  //If not, error
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello!"));
-
-//Passport middleware
+// Passport middleware
 app.use(passport.initialize());
-require("./config/passport")(passport);
 
-//Routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
